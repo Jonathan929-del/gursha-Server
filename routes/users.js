426c3ae26571e09a1a0c96ca9ed0695d0dcee0d6
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2023-04-17 11:45:11 +0200

    Fixed errors.

--- routes/users.js
+++ routes/users.js
@@ -30,7 +30,7 @@ router.post('/register', async (req, res) => {
             const hashedPassword = bcrypt.hashSync(password);
             const newUser = await User.create({
                 username,
-                email:'test@test.com',
+                email,
                 password:hashedPassword,
                 bio:'',
                 profilePic:'',
